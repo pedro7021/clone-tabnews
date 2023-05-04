e96b90201726aba2bd857e661b245854300d1a1c
Pedro de Carli <32084353+pedro7021@users.noreply.github.com>
2023-05-04 19:38:24 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import gatinho from "../assets/gatinho.webp";
+
+function Home() {
+    return <Image src={gatinho} alt="Gatinhos lutando com sabre de luz" fill="true"/>
+}
+
+export default Home;
