4f8194fe7d9c31c5e17467ea4a79627fac420412
Pedro de Carli <32084353+pedro7021@users.noreply.github.com>
2023-06-27 17:01:47 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 import gatinho from "../assets/gatinho.webp";
 
 function Home() {
-    return <Image src={gatinho} alt="Gatinhos lutando com sabre de luz" fill="true"/>
+  return (
+    <Image src={gatinho} alt="Gatinhos lutando com sabre de luz" fill="true" />
+  );
 }
 
 export default Home;
