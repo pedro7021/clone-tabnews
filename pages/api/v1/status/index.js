074df2c2ab5cd29c0d4fe79e46007637f5da6766
Pedro de Carli <32084353+pedro7021@users.noreply.github.com>
2023-11-27 18:00:30 +0000

    add `database.js`and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1;");
   response.status(200).json({ mensagem: "hello world!!" });
   response;
 }
